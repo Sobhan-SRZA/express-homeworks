20a04403b1030c7e09e27c323eb22124a0d07a5d
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-06 10:00:45 +0330

    Update Application.tsx

--- lesson 3 - chat platform with react/frontend/src/pages/Application.tsx
+++ lesson 3 - chat platform with react/frontend/src/pages/Application.tsx
@@ -1,5 +1,23 @@
-export default function Application() {
+import useWebSocket from "../hooks/useWebsocket"
+import backend from "../backend/backend"
+import { useState } from "react";
+
+interface ApplicationProbs {
+  token: string;
+}
+
+export default function Application({ token }: ApplicationProbs) {
+  const { } = useWebSocket(`${backend.websocket}?token=${token}`);
+
+  const [openChat, setOpenChat] = useState<boolean>(false);
+
+  const chats = [
+    { name: "mmd", last_message: { text: "sexy boy", timestamp: 1778047725255 } }
+  ]
+
   return (
-    <div>Application</div>
+    <>
+
+    </>
   )
 }
