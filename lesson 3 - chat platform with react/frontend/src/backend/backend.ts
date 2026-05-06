4d5d12cc29764c61930545911efc273799a5616a
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-06 10:00:32 +0330

    Update backend.ts

--- lesson 3 - chat platform with react/frontend/src/backend/backend.ts
+++ lesson 3 - chat platform with react/frontend/src/backend/backend.ts
@@ -1,5 +1,8 @@
 const url = "http://localhost:8888";
 
 export default {
-    login: `${url}/api/login`
+    login: `${url}/api/login`,
+    register: `${url}/api/register`,
+    auth: `${url}/api/users/me`,
+    websocket: `http://localhost:3000/ws`
 } as const;
