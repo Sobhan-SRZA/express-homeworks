99c917ba76a916b31c2a7478ddd0852655d2884a
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-06 16:48:21 +0330

    Update useWebsocket.tsx

--- lesson 3 - chat platform with react/frontend/src/hooks/useWebsocket.tsx
+++ lesson 3 - chat platform with react/frontend/src/hooks/useWebsocket.tsx
@@ -1,19 +1,30 @@
 import { useState, useEffect, useRef } from 'react';
 
+interface SocketMessage {
+    type: string;
+    code: string;
+    message: string;
+    payload?: object;
+}
+
 export default function useWebSocket(url: string) {
-    const [messages, setMessages] = useState<any[]>([]);
+    const [messages, setMessages] = useState<SocketMessage[]>([]);
     const socketRef = useRef<WebSocket>(null);
 
     useEffect(() => {
         socketRef.current = new WebSocket(url);
 
         socketRef.current.onmessage = (event) => {
+            const data = event.data;
+
+            if (data.type === "error") {
+                if (["AUTH_EXPIRE", "AUTH_MISSING"].includes(data.code)) {
+                    location.reload();
+                }
+            }
+            
             setMessages(prev => [...prev, event.data]);
         };
-
-        // socketRef.current.onerror = (error) => {
-        //     console.error('WebSocket error:', error);
-        // };
 
         return () => {
             socketRef.current?.close();
