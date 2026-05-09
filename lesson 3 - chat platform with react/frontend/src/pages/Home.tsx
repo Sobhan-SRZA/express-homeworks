236b3634c3e4e7d935c027d2b23d6050df74464e
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-09 05:30:30 +0330

    Update Home.tsx

--- lesson 3 - chat platform with react/frontend/src/pages/Home.tsx
+++ lesson 3 - chat platform with react/frontend/src/pages/Home.tsx
@@ -59,11 +59,14 @@ export default function Home() {
         <>
             {(isLoggined && token)
                 && <Application token={token} />
-                || <section id="singin-form" className="w-max flex flex-col gap-4 text-center items-center justify-center self-center bg-(--sec-bg) rounded-2xl p-10">
-                    <h2 className="font-bold text-2xl">Segraph Best Chat Platform</h2>
+                ||
+                <main className="grow custom-container mx-auto flex flex-col justify-center text-center px-4 py-8">
+                    <section id="singin-form" className="w-max flex flex-col gap-4 text-center items-center justify-center self-center bg-(--sec-bg) rounded-2xl p-10">
+                        <h2 className="font-bold text-2xl">Segraph Best Chat Platform</h2>
 
-                    <SignInForm />
-                </section>}
+                        <SignInForm />
+                    </section>
+                </main>}
         </>
     )
 }
