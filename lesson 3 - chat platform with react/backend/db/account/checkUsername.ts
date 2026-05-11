5ab0aa34e4a3eb8fc9e6ee4b550df969f2140bb2
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-11 18:18:16 +0330

    Update checkUsername.ts

--- lesson 3 - chat platform with react/backend/db/account/checkUsername.ts
+++ lesson 3 - chat platform with react/backend/db/account/checkUsername.ts
@@ -1,10 +1,9 @@
-import { Database } from "../../types/database";
 import getData from "../../database/commands/getData";
 
 export default (username: string) => {
-    const accounts = getData("accounts") as Database[];
+    const accounts = getData("accounts");
 
-    if (!accounts || accounts.length < 1)
+    if (!accounts || !Array.isArray(accounts) || accounts.length < 1)
         return false;
 
     const usernames = accounts.map(a => a.value.username.toLowerCase());
