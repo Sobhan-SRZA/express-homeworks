88356c5a54a14b52c5f1b82203527637992c5816
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-11 18:18:09 +0330

    Create types.ts

---
+++ lesson 3 - chat platform with react/backend/database/commands/types.ts
@@ -0,0 +1,56 @@
+import tables from "./tables";
+
+export type Tables = typeof tables[number];
+
+export type TableDataMap = {
+    [K in Tables]: K extends "users" ? User :
+    K extends "accounts" ? Account :
+    K extends "messages" ? Message :
+    K extends "chats" ? Chat :
+    K extends "files" ? File :
+    never;
+};
+
+export type Table = keyof TableDataMap;
+
+export interface Account {
+    created_at: number;
+    username: string;
+    id: string;
+    password: string;
+}
+
+export interface Message {
+    messageId: number;
+    from: string;
+    to: string;
+    text: string;
+    timestamp: string;
+    status: "sent" | "deliverd" | "seen";
+    sentToUser: boolean;
+    deliveredAt: number | null;
+    seenAt: number | null;
+}
+
+export interface User {
+    userId: string;
+    online: boolean;
+    lastSeen: string | null;
+}
+
+export interface Chat { }
+
+export interface File {
+    originalName: string;
+    storedName: string;
+    filePath: string;
+    type: string;
+    size: number;
+    uploadedBy: string;
+    uploadTime: number;
+}
+
+export interface DatabaseEntry<T> {
+    id: string;
+    value: T;
+}
