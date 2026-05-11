d5611c76cf0a5b94599fc21564951a3ac261d5b6
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-11 18:18:38 +0330

    Update getFileRecord.ts

--- lesson 3 - chat platform with react/backend/db/files/getFileRecord.ts
+++ lesson 3 - chat platform with react/backend/db/files/getFileRecord.ts
@@ -1,23 +1,2 @@
-const getData = require("../../database/commands/getData");
-
-/**
- * 
- * @param {string} id 
- * @returns {{originalName: string storedName: string filePath: string type: string size: size uploadedBy: string uploadTime: Date} | null}
- */
-module.exports = function (id) {
-    try {
-        const file = getData("files", id);
-
-        if (!file)
-            throw Error("File with " + id + " ID didn't founded!");
-
-        return file.value;
-    }
-
-    catch (error) {
-        console.log("Error getting file from database:", error)
-
-        return null;
-    }
+export default function (id:string) {
 }
