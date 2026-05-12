2592598c825c9cc7dbc95b43827c902ab2a2136c
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:42:22 +0330

    Update users.ts

--- lesson 3 - chat platform with react/backend/api/users.ts
+++ lesson 3 - chat platform with react/backend/api/users.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
+import authenticateToken from "../middleware/authHandler";
+import { RequestRouter } from "../types/requests";
 
 const router = Router();
 
-router.get('/me', authenticateToken, (req, res) => {
+router.get('/me', authenticateToken, (req: RequestRouter, res) => {
     const userProfile = {
-        id: req.user.id,
-        username: req.user.username,
-        created_at: req.user.created_at,
+        id: req.user!.id,
+        username: req.user!.username,
+        created_at: req.user!.created_at,
     };
 
     return res
