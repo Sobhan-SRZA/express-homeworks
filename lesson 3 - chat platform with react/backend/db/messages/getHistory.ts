68f4843dd4ca47471bae3651423b23bf8d4b017c
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:18:51 +0330

    Update getHistory.ts

--- lesson 3 - chat platform with react/backend/db/messages/getHistory.ts
+++ lesson 3 - chat platform with react/backend/db/messages/getHistory.ts
@@ -1,16 +1,17 @@
-const getData = require("../../database/commands/getData");
-const chatId = require("../../utils/chatId");
+import getData from "../../database/commands/getData";
+import chatId from "../../utils/chatId";
 
-/**
- * 
- * @param {string} u1 
- * @param {string} u2 
- * @returns {{ messageId: number from: string to: string text: string timestamp: string status: { sentToUser: boolean deliveredToUser: boolean seenByuser: boolean } deliveredAt: string | null seenAt: string | null }[]}
- */
-module.exports = function (u1, u2) {
+export default function (u1: string, u2: string) {
+    try {
+        const cid = chatId(u1, u2);
+        const messages = getData("messages", cid)?.value || [];
 
-    const cid = chatId(u1, u2);
-    const messages = getData("messages", cid).value || [];
+        return messages;
+    }
 
-    return messages;
+    catch (e) {
+        console.error("Getting chat history has problem:", e);
+
+        return null;
+    }
 }
