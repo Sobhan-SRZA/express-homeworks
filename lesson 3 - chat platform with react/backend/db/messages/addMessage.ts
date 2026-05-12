489c8ec61944b20a1c4343b0d7f73d169b19fc12
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:18:47 +0330

    Update addMessage.ts

--- lesson 3 - chat platform with react/backend/db/messages/addMessage.ts
+++ lesson 3 - chat platform with react/backend/db/messages/addMessage.ts
@@ -1,32 +1,42 @@
+import { Message } from "../../database/commands/types";
 import getData from "../../database/commands/getData";
 import chatId from "../../utils/chatId";
 
 export default function (from: string, to: string, text: string) {
-    const cid = chatId(from, to);
-    let messages = getData("messages", cid);
+    try {
+        const cid = chatId(from, to);
+        let messages = getData("messages", cid);
 
-    const message = {
-        messageId: Date.now(),
-        from,
-        to,
-        text,
-        timestamp: new Date().toISOString(),
-        sentToUser: false,
-        status: "sent",
-        deliveredAt: null,
-        seenAt: null
-    };
+        const currentTime = Date.now();
+        const message: Message = {
+            messageId: currentTime,
+            from,
+            to,
+            text,
+            timestamp: currentTime,
+            status: "sent",
+            sentAt: null,
+            deliveredAt: null,
+            seenAt: null
+        };
 
-    if (!messages || !messages.value || messages.value.length < 1) {
-        messages = {
-            id: cid,
-            value: []
+        if (!messages || !messages.value || messages.value.length < 1) {
+            messages = {
+                id: cid,
+                value: []
+            }
         }
+
+        messages.value.push(message);
+
+        setData("messages", messages, cid);
+
+        return message;
     }
 
-    messages.value.push(message);
+    catch (e) {
+        console.error("Sending message has problem:", e);
 
-    setData("messages", messages, cid);
-
-    return message;
+        return null;
+    }
 }
