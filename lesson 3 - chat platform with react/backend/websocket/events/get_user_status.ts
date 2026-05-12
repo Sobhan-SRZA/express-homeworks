7e1496ef3473dbbc1dc9d702c47032be6c128106
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:19:27 +0330

    Update get_user_status.ts

--- lesson 3 - chat platform with react/backend/websocket/events/get_user_status.ts
+++ lesson 3 - chat platform with react/backend/websocket/events/get_user_status.ts
@@ -1,19 +1,13 @@
-const getUserStatus = require("../../db/users/getUserStatus");
+import { CustomSocket } from "../../types/requests";
+import getUserStatus from "../../db/users/getUserStatus";
 
-/**
- * 
- * @param {WebSocket} ws 
- * @param {{ payload: { userId: string } }} parsedMessage 
- * @returns {void}
- */
-module.exports = async (ws, parsedMessage) => {
-    const { userId } = parsedMessage.payload;
+export default (socket: CustomSocket, payload: any) => {
+    const { userId } = payload;
     const status = getUserStatus(userId);
 
-    ws.send(JSON.stringify({
-        type: "user_status",
-        payload: status
-    }));
+    socket.emit("user_status", {
+        status
+    });
 
     return;
 }
