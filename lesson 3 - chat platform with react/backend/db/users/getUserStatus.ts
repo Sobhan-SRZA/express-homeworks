8537d42a2a6d657bd6a87b6b5481b121e3777ac0
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:18:59 +0330

    Update getUserStatus.ts

--- lesson 3 - chat platform with react/backend/db/users/getUserStatus.ts
+++ lesson 3 - chat platform with react/backend/db/users/getUserStatus.ts
@@ -1,18 +1,26 @@
-import { User } from "../../database/commands/types";
+import { UserStatus } from "../../database/commands/types";
 import getAccount from "../account/getAccount";
 import getData from "../../database/commands/getData";
 
-export default function (userId: string): User {
-    const account = getAccount(userId);
-    if (!account) {
-        throw "Account was not found."
+export default function (userId: string): UserStatus | null {
+    try {
+        const account = getAccount(userId);
+        if (!account) {
+            throw "Account was not found."
+        }
+
+        const users = getData("users");
+        const userStatusData = users.find(a => a.id === `${account.id}`);
+
+        const online = userStatusData?.value?.online || false;
+        const lastSeen = userStatusData?.value?.lastSeen || null;
+
+        return { userId, online, lastSeen };
     }
 
-    const users = getData("users");
-    const userStatusData = users.find(a => a.id === `${account.id}`);
+    catch (e) {
+        console.error("Getting the user status has problem:", e)
 
-    const online = userStatusData?.value?.online || false;
-    const lastSeen = userStatusData?.value?.lastSeen || null;
-
-    return { userId, online, lastSeen };
+        return null;
+    }
 }
