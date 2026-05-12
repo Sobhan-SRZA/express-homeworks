2abce50d8eb802b837984aaf4afadb602e252ad6
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:19:30 +0330

    Update open_chat.ts

--- lesson 3 - chat platform with react/backend/websocket/events/open_chat.ts
+++ lesson 3 - chat platform with react/backend/websocket/events/open_chat.ts
@@ -1,28 +1,23 @@
-const getAccount = require("../../db/account/getAccount");
-const getUserStatus = require("../../db/users/getUserStatus");
+import { UserTokenVerify } from "../../types/user";
+import { CustomSocket } from "../../types/requests";
+import getAccount from "../../db/account/getAccount";
+import getUserStatus from "../../db/users/getUserStatus";
 
-/**
- * 
- * @param {WebSocket} ws 
- * @param {{ payload: { userId: string } }} parsedMessage 
- * @param {string} senderId 
- * @param {{ username: string }} currentUser 
- * @returns {void}
- */
-module.exports = async (ws, parsedMessage, senderId, currentUser) => {
-    const { userId } = parsedMessage.payload;
+export default (socket: CustomSocket, payload: any, senderId: string, currentUser: UserTokenVerify) => {
+    const { userId } = payload;
     console.log(`user ${currentUser.username} is oppend chat room with ${userId}.`);
-    const targetUser = await getAccount(userId);
-    const status = await getUserStatus(userId);
+
+    const targetUser = getAccount(userId);
+    const status = getUserStatus(userId);
 
     const user = {
-        id: targetUser.id,
-        username: targetUser.username,
-        created_at: targetUser.created_at,
+        id: targetUser!.id,
+        username: targetUser!.username,
+        created_at: targetUser!.created_at,
         status
     }
 
-    ws.send(JSON.stringify({ type: 'chat_opened', payload: user }));
+    socket.emit('chat_opened', { user });
 
     return;
 }
