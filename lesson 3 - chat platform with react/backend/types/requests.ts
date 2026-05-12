bcc4d350a165d7875799f81f27b43a5523b11fd3
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-12 17:19:15 +0330

    Update requests.ts

--- lesson 3 - chat platform with react/backend/types/requests.ts
+++ lesson 3 - chat platform with react/backend/types/requests.ts
@@ -1,6 +1,11 @@
 import { UserTokenVerify } from "./user";
 import { Request } from "express";
+import { Socket } from "socket.io";
 
-export interface RequestRouter extends Request{
+export interface RequestRouter extends Request {
+    user?: UserTokenVerify;
+}
+
+export interface CustomSocket extends Socket {
     user?: UserTokenVerify;
 }
