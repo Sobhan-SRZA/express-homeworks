e10c6a2c86adba8dfa31e61491a87b1958a3fdaa
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-13 18:32:03 +0330

    Update vite.config.ts

--- lesson 3 - chat platform with react/frontend/vite.config.ts
+++ lesson 3 - chat platform with react/frontend/vite.config.ts
@@ -8,12 +8,4 @@ export default defineConfig({
     react(),
     tailwindcss()
   ]
-})
-/**
- * @copyright
- * Code by Sobhan-SRZA (mr.sinre) | https://github.com/Sobhan-SRZA
- * Developed for Persian Caesar | https://github.com/Persian-Caesar | https://dsc.gg/persian-caesar
- *
- * If you encounter any issues or need assistance with this code,
- * please make sure to credit "Persian Caesar" in your documentation or communications.
- */
+});
