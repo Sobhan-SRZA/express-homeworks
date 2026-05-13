59168749e13407fb60519c39b3f193aeb5c567e8
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-13 17:35:50 +0330

    Update NotFound.tsx

--- lesson 3 - chat platform with react/frontend/src/pages/NotFound.tsx
+++ lesson 3 - chat platform with react/frontend/src/pages/NotFound.tsx
@@ -1,58 +1,18 @@
 import {
-    NavLink,
     useLocation,
     useNavigate
 } from "react-router-dom";
-import {
-    useEffect,
-    useState
-} from "react";
-import { Helmet } from "react-helmet";
+import { useEffect } from "react";
 
 const NotFound: React.FC = () => {
     const navigate = useNavigate();
     const location = useLocation();
 
-    const [pathLoc, setPathLoc] = useState(location.pathname);
-
     useEffect(() => {
-        if (location.pathname !== "/404") {
-            setPathLoc(location.pathname)
-            navigate("/404", { replace: true });
-        }
+        navigate("/", { replace: true });
     }, [location.pathname, navigate]);
 
-    return (
-        <>
-            <Helmet>
-                <title>Segraph | Page Not Found</title>
-            </Helmet>
-
-            <main className="grow custom-container mx-auto flex flex-col justify-center text-center px-4 py-8">
-                <div className="text-center">
-                    <p className="text-7xl font-semibold text-(--primary) font-iransans">404</p>
-                    <h1 className="mt-4 text-4xl sm:text-5xl font-semibold tracking-tight text-(--text) font-iransans">
-                        {"Page Not Be Founded"}
-                    </h1>
-
-                    <p className="mt-6 text-lg font-medium text-(--text)/70 font-iransans sm:text-xl">
-                        {"Sorry, the page you are looking for does not exist."}
-                        {pathLoc}
-                    </p>
-
-                    <div className="mt-10 flex items-center justify-center gap-x-6">
-                        <NavLink
-                            to="/"
-                            className="rounded-md bg-(--primary) px-3.5 py-2.5 text-sm font-semibold text-white font-iransans shadow-sm hover:bg-(--primary-hover) focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-(--primary transition-all"
-                            aria-label={"Back to Home"}
-                        >
-                            {"Back to Home"}
-                        </NavLink>
-                    </div>
-                </div>
-            </main>
-        </>
-    );
+    return;
 };
 
 export default NotFound;
