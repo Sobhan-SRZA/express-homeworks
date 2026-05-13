3802603c315db5ec0902021396da773a1c10f045
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-13 18:31:58 +0330

    Update Home.tsx

--- lesson 3 - chat platform with react/frontend/src/pages/Home.tsx
+++ lesson 3 - chat platform with react/frontend/src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from "react";
+import {
+    useEffect,
+    useState
+} from "react";
+import Application from "./Application";
 import SignInForm from "../components/SignInForm";
 import backend from "../backend/backend";
-import Application from "./Application";
 
 export default function Home() {
     const [isLoggined, setIsLoggined] = useState(false);
@@ -70,12 +73,3 @@ export default function Home() {
         </>
     )
 }
-
-/**
- * @copyright
- * Code by Sobhan-SRZA (mr.sinre) | https://github.com/Sobhan-SRZA
- * Developed for Persian Caesar | https://github.com/Persian-Caesar | https://dsc.gg/persian-caesar
- *
- * If you encounter any issues or need assistance with this code,
- * please make sure to credit "Persian Caesar" in your documentation or communications.
- */
