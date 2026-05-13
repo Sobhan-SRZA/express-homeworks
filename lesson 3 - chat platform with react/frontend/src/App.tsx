3d54b88fa183ed5fc5a6ba11761d1b74483211d0
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-13 17:39:10 +0330

    Update App.tsx

--- lesson 3 - chat platform with react/frontend/src/App.tsx
+++ lesson 3 - chat platform with react/frontend/src/App.tsx
@@ -45,14 +45,4 @@ const App: React.FC = () => {
   );
 };
 
-// Export the App component as the default export.
 export default App;
-
-/**
- * @copyright
- * Code by Sobhan-SRZA (mr.sinre) | https://github.com/Sobhan-SRZA
- * Developed for Persian Caesar | https://github.com/Persian-Caesar | https://dsc.gg/persian-caesar
- *
- * If you encounter any issues or need assistance with this code,
- * please make sure to credit "Persian Caesar" in your documentation or communications.
- */
