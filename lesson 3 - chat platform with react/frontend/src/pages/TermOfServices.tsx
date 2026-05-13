b31f4f4b5c9ef36ce3426c09e946adde1292061d
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-13 17:35:54 +0330

    Delete TermOfServices.tsx

--- lesson 3 - chat platform with react/frontend/src/pages/TermOfServices.tsx
+++
@@ -1,7 +0,0 @@
-export default function TermOfServices() {
-  return (
-    <main className="grow custom-container mx-auto flex flex-col justify-center text-center px-4 py-8">
-      <div>TermOfServices</div>
-    </main>
-  )
-}
