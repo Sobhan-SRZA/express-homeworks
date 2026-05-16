7e08c4e8e9529bd237904d05c354bb7ba5d7daa7
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-05-16 16:19:18 +0330

    Create DisplayUserAvatar.tsx

---
+++ lesson 3 - chat platform with react/frontend/src/components/chat/DisplayUserAvatar.tsx
@@ -0,0 +1,15 @@
+import type { UserChat } from "./ChatsList";
+
+export default function DisplayUserAvatar({ chat }: {
+    chat: UserChat;
+}) {
+    const userAvatarName = `${chat.name.toUpperCase().split(" ")[0].toString()[0]}${chat.name.toUpperCase().split(" ")[1]?.toString()![0] || ""}`
+
+    return (
+        chat.avatar
+        && <img src={chat.avatar} alt="" className="size-13 rounded-full" />
+        || <div className="size-13 rounded-full bg-(--primary) text-(--background) font-bold text-2xl content-center">
+            <p>{userAvatarName}</p>
+        </div>
+    )
+}
