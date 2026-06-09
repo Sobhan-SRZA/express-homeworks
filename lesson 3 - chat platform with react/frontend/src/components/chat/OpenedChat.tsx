9a92d110ce92dc5111cea99d7a785acd532d6ac4
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-09 20:18:20 +0330

    Update OpenedChat.tsx

--- lesson 3 - chat platform with react/frontend/src/components/chat/OpenedChat.tsx
+++ lesson 3 - chat platform with react/frontend/src/components/chat/OpenedChat.tsx
@@ -1,9 +1,23 @@
+import type { ClientToServerEvents, EventPayload } from "../../hooks/useWebsocket";
+
 export default function OpenedChat(
-  { id }: {
-    id: string
+  { id, emitEvent }: {
+    id: string;
+    emitEvent: <E extends keyof ClientToServerEvents>(event: E, payload?: EventPayload<ClientToServerEvents[E]>) => boolean;
   }
 ) {
   return (
-    <div>OpenedChat</div>
+    <div
+      onClick={() => {
+        emitEvent("event", {
+          type: "send_message",
+          payload: {
+            originalMessageId: Date.now().toString(),
+            text: "se kon",
+            to: id
+          }
+        })
+      }}
+    >OpenedChat</div>
   )
 }
