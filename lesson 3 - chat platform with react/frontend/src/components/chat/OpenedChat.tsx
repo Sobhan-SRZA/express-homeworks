3e4efc4e5ca9317f33e3d2a7c108db250d33696d
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-12 07:11:16 +0330

    Update OpenedChat.tsx

--- lesson 3 - chat platform with react/frontend/src/components/chat/OpenedChat.tsx
+++ lesson 3 - chat platform with react/frontend/src/components/chat/OpenedChat.tsx
@@ -2,6 +2,10 @@ import type {
   ClientToServerEvents,
   EventPayload
 } from "../../hooks/useWebsocket";
+import {
+  Send,
+  SendHorizonal
+} from "lucide-react";
 import { useRef } from "react";
 
 export default function OpenedChat(
@@ -25,9 +29,34 @@ export default function OpenedChat(
   }
 
   return (
-    <div>
-      <textarea name="message" id="message" ref={message}></textarea>
-      <button onClick={sendMessage} form="message">telegram</button>
+    <div className="flex flex-col justify-between">
+      {/* Chat history */}
+      <div>
+
+      </div>
+
+
+      {/* Sendding part */}
+      <div className="bg-(--lgs-bg) rounded-4xl flex gap-3 p-2 place-self-center items-center w-[70%] flex-2">
+        <textarea
+          name="message"
+          id="message"
+          ref={message}
+          dir="auto"
+          placeholder="پیام بنویسید..."
+          className="empty:h-8 w-full rtl outline-0 bg-transparent placeholder:text-(--text)/50"
+        />
+        <button
+          onClick={sendMessage}
+          form="message"
+          disabled={(message.current?.value.length || 0) < 1}
+          className="cursor-pointer bg-(--hover) rounded-full size-10 disabled:opacity-60 disabled:cursor-not-allowed"
+        >
+          <SendHorizonal
+            className="size-[70%] place-self-center"
+          />
+        </button>
+      </div>
     </div>
   )
 }
