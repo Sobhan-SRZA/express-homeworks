32b1f75d6459252d55996b76fb43e48818092497
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-17 20:43:16 +0330

    Update types.d.ts

--- lesson 3 - chat platform with react/frontend/src/components/chat/types.d.ts
+++ lesson 3 - chat platform with react/frontend/src/components/chat/types.d.ts
@@ -16,3 +16,36 @@ export interface OpenChatProbs {
         payload?: EventPayload<ClientToServerEvents[E]>
     ) => boolean;
 }
+
+export interface ChatMessageProbs {
+    chat: UserChat;
+    onSelect: ChatListProbs["onSelect"];
+}
+
+export interface UserChat {
+    id: string;
+    last_seen: number;
+    muted: boolean;
+    name: string;
+    last_message: {
+        text: string;
+        timestamp: number;
+    };
+    status: string;
+    unread_message: number;
+    avatar?: string;
+}
+
+export interface ChatListProbs {
+    chats: UserChat[];
+    onSelect: React.Dispatch<React.SetStateAction<OpenChatState>>;
+}
+
+export interface ChatProbs {
+    chat: UserChat;
+    setChat: OpenChatProbs["setChat"];
+    messages: MessageType[];
+    currentUserId: OpenChatProbs["currentUserId"];
+    setMessages: React.Dispatch<React.SetStateAction<ChatProbs["messages"]>>;
+    emitEvent: OpenChatProbs["emitEvent"];
+}
