c853b9412c11c9adb5ca1d9043c79ccb87585230
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-20 12:39:51 +0330

    Update useGetChats.tsx

--- lesson 3 - chat platform with react/frontend/src/backend/websocket/useGetChats.tsx
+++ lesson 3 - chat platform with react/frontend/src/backend/websocket/useGetChats.tsx
@@ -1,7 +1,18 @@
 import type { UseGetChatsOptions } from "./type";
+import { useCallback } from "react";
 
 export default function useWebSocket({
     socket
 }: UseGetChatsOptions) {
+    const updateOpenChatMessages = useCallback(() => {
+        socket.on("chat_history", (data) => {
+            console.log("chat_history.data", data)
+        });
+    }, []);
 
+
+
+    return {
+
+    }
 }
