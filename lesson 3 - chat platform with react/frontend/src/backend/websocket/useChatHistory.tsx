64833cf76557b6ea4ca650a591b5b930add4b1ea
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-20 13:01:01 +0330

    Update useChatHistory.tsx

--- lesson 3 - chat platform with react/frontend/src/backend/websocket/useChatHistory.tsx
+++ lesson 3 - chat platform with react/frontend/src/backend/websocket/useChatHistory.tsx
@@ -1,18 +1,35 @@
 import type { UseChatHistorysOptions } from "./type";
-import { useCallback } from "react";
+import {
+    useCallback,
+    useState
+} from "react";
+import type { History } from "../../components/message/types";
 
 export default function useWebSocket({
-    socket
+    socket,
+    emitEvent
 }: UseChatHistorysOptions) {
-    const updateOpenChatMessages = useCallback(() => {
+    const [history, setHistory] = useState<History | null>(null);
+
+    const updateHistory = useCallback((userId: string) => {
+        emitEvent("event", {
+            type: "get_chat_history",
+            payload: { with: userId }
+        })
+    }, [emitEvent]);
+
+    const getHistory = useCallback(() => {
         socket.on("chat_history", (data) => {
             console.log("chat_history.data", data)
+            setHistory(data.messages)
         });
     }, []);
 
 
 
     return {
-
+        history,
+        updateHistory,
+        getHistory
     }
 }
