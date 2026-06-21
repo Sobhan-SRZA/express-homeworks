c77c5246b9148338f07b15a6750507ca5d3dee7f
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-21 17:44:05 +0330

    Update ChatsList.tsx

--- lesson 3 - chat platform with react/frontend/src/components/chat/ChatsList.tsx
+++ lesson 3 - chat platform with react/frontend/src/components/chat/ChatsList.tsx
@@ -11,6 +11,7 @@ export default function ChatsList({
                 .map((chat) => {
                     return (
                         <ChatMessage
+                            key={chat.id}
                             chat={chat}
                             onSelect={onSelect}
                         />
