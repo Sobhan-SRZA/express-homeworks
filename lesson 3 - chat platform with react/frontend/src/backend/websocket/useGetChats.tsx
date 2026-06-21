5e7260bfbaaeabf32472dfb3d23155a4724f9135
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-21 17:44:09 +0330

    Update useGetChats.tsx

--- lesson 3 - chat platform with react/frontend/src/backend/websocket/useGetChats.tsx
+++ lesson 3 - chat platform with react/frontend/src/backend/websocket/useGetChats.tsx
@@ -1,9 +1,12 @@
-import type { Chats, UseGetChatsOptions } from "./type";
+import { io } from "socket.io-client";
+import type { AppSocket, Chats, UseGetChatsOptions } from "./type";
 import { useCallback, useState } from "react";
 
 export default function useGetChats({
     socket,
-    emitEvent
+    emitEvent,
+    url,
+    token,
 }: UseGetChatsOptions) {
     const [chats, setChats] = useState<Chats | null>(null);
 
@@ -14,11 +17,27 @@ export default function useGetChats({
     }, [emitEvent]);
 
     const updateOpenChats = useCallback(() => {
+        if (!socket) {
+            socket = io(url, {
+                transports: ["websocket"],
+                auth: { token },
+                reconnection: true,
+                reconnectionAttempts: 5,
+                reconnectionDelay: 1000,
+                reconnectionDelayMax: 5000,
+                timeout: 10000
+            });
+
+            return;
+        }
+
         socket.on("openned_chats", (data) => {
             console.log("🚀 ~ useGetChats ~ data:", data)
 
             setChats(data.chats)
         });
+
+        return;
     }, [socket]);
 
     return {
