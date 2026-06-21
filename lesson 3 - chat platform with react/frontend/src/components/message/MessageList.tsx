bafc46a8ea3ca51f29e2fd8afad75adff278fea4
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-22 00:17:50 +0330

    Update MessageList.tsx

--- lesson 3 - chat platform with react/frontend/src/components/message/MessageList.tsx
+++ lesson 3 - chat platform with react/frontend/src/components/message/MessageList.tsx
@@ -11,17 +11,24 @@ export default function MessageList({
             ref={ref}
             className="flex-1 overscroll-y-contain overflow-y-scroll custom-scroll max-h-screen scroll-smooth p-4 py-20 space-y-3"
         >
-            {messages.map((msg) => {
-                const isOwn = msg.from === from;
+            {
+                (messages && messages.length > 0) &&
 
-                return (
-                    <Message
-                        key={msg.id}
-                        isOwnMessage={isOwn}
-                        message={msg}
-                    />
-                );
-            })}
+                messages.map((msg) => {
+                    const isOwn = msg.from === from;
+
+                    return (
+                        <Message
+                            key={msg.id}
+                            isOwnMessage={isOwn}
+                            message={msg}
+                        />
+                    );
+                })
+
+                ||
+                <p>پیامی وجود ندارد</p>
+            }
         </div>
     )
 }
