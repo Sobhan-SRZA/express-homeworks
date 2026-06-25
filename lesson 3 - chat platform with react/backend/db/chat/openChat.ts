a3f7b2968528bcfae1a4a92227ebe1a7e2ee4904
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-06-25 21:28:29 +0330

    Update openChat.ts

--- lesson 3 - chat platform with react/backend/db/chat/openChat.ts
+++ lesson 3 - chat platform with react/backend/db/chat/openChat.ts
@@ -5,56 +5,10 @@ import getHistory from "../messages/getHistory";
 
 export default (startId: string, nextId: string) => {
     try {
-        const userAccount = getData("accounts", nextId);
+        const chatData = setChats(startId, nextId);
+        const nextChatData = setChats(nextId, startId);
 
-        if (!userAccount) {
-            throw ("Account with " + nextId + " ID didn't founded!");
-        }
-
-        const starterAccount = getData("accounts", startId);
-
-        if (!starterAccount) {
-            throw ("Account with " + startId + " ID didn't founded!");
-        }
-
-        const chats = getData("chats", startId);
-
-        const messages = getHistory(starterAccount.value.id, userAccount.value.id);
-
-        const data: Chat = {
-            userId: nextId,
-            username: userAccount.value.username,
-            unread_messages: messages?.filter(a => a.from === nextId && a.status !== "read").length || 0,
-            last_message: messages?.reverse()[0] || null
-        };
-
-        const chatIndex = chats?.value?.findIndex(chat => `${chat.userId}` === `${nextId}`);
-        if (chats && !isNaN(chatIndex!) && chatIndex !== -1) {
-            const chat = chats.value[chatIndex!];
-
-            chat.last_message = data.last_message;
-            chat.unread_messages = data.unread_messages;
-            console.log("🚀 ~ chat:", chat)
-
-            setData(
-                "chats",
-                chats,
-                startId
-            )
-        }
-
-        else {
-            setData(
-                "chats",
-                {
-                    id: startId,
-                    value: chats && chats.value ? [...chats.value, data] : [data]
-                },
-                startId
-            )
-        }
-
-        return data;
+        return chatData;
     }
 
     catch (e) {
@@ -62,4 +16,57 @@ export default (startId: string, nextId: string) => {
 
         return null;
     }
+}
+
+function setChats(id1: string, id2: string) {
+    const userAccount = getData("accounts", id2);
+
+    if (!userAccount) {
+        throw ("Account with " + id2 + " ID didn't founded!");
+    }
+
+    const starterAccount = getData("accounts", id1);
+
+    if (!starterAccount) {
+        throw ("Account with " + id1 + " ID didn't founded!");
+    }
+
+    const chats = getData("chats", id1);
+
+    const messages = getHistory(starterAccount.value.id, userAccount.value.id);
+
+    const data: Chat = {
+        userId: id2,
+        username: userAccount.value.username,
+        unread_messages: messages?.filter(a => a.from === id2 && a.status !== "read").length || 0,
+        last_message: messages?.reverse()[0] || null
+    };
+
+    const chatIndex = chats?.value?.findIndex(chat => `${chat.userId}` === `${id2}`);
+    if (chats && !isNaN(chatIndex!) && chatIndex !== -1) {
+        const chat = chats.value[chatIndex!];
+
+        chat.last_message = data.last_message;
+        chat.unread_messages = data.unread_messages;
+        console.log("🚀 ~ chat:", chat)
+
+        setData(
+            "chats",
+            chats,
+            id1
+        )
+    }
+
+    else {
+        setData(
+            "chats",
+            {
+                id: id1,
+                value: chats && chats.value ? [...chats.value, data] : [data]
+            },
+            id1
+        )
+    }
+
+    return data;
 }
